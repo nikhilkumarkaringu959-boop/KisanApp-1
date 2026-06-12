50248d5ee3cec8c3569dd22b020ba286c7a27d5c
nikhilkumarkaringu929 <nikhilkumarkaringu929@gmail.com>
2026-06-12 11:39:55 +0530

    Create ProfileContext.tsx

---
+++ Context/ProfileContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState({
+    language: '', name: '', age: '', gender: '', state: '',
+    district: '', mandal: '', village: '', landSize: '', soilType: ''
+  });
+
+  useEffect(() => { loadProfile(); }, []);
+
+  const loadProfile = async () => {
+    const data = await AsyncStorage.getItem('kisanProfile');
+    if (data) setProfile(JSON.parse(data));
+  };
+
+  const updateProfile = async (key, value) => {
+    const newProfile = {...profile, [key]: value };
+    setProfile(newProfile);
+    await AsyncStorage.setItem('kisanProfile', JSON.stringify(newProfile));
+  };
+
+  return (
+    <ProfileContext.Provider value={{ profile, updateProfile }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => useContext(ProfileContext);
