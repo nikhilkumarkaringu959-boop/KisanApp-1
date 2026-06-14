683523b6ccfb3786755b91c8b9bf93ac99557b72
nikhilkumarkaringu929 <nikhilkumarkaringu929@gmail.com>
2026-06-14 08:35:01 +0530

    Create _layot.js

---
+++ App/_layot.js
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="screens/LanguageScreen" />
+      <Stack.Screen name="screens/FarmerProfile" />
+    </Stack>
+  );
+}
