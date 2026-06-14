063208bfc728c5e4f1b29bea68970f266a227574
nikhilkumarkaringu929 <nikhilkumarkaringu929@gmail.com>
2026-06-14 08:50:16 +0530

    Update _layot.js

--- App/_layot.js
+++ App/_layot.js
@@ -7,6 +7,12 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="screens/LanguageScreen" />
       <Stack.Screen name="screens/FarmerProfile" />
+      <Stack.Screen name="screens/CropInfo" />
+      <Stack.Screen name="screens/Weather" />
+      <Stack.Screen name="screens/FertilizerCalc" />
+      <Stack.Screen name="screens/PestControl" />
+      <Stack.Screen name="screens/FarmingTips" />
+      <Stack.Screen name="screens/GovtSchemes" />
     </Stack>
   );
 }
